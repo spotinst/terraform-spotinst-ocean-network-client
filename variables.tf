ea21ba6401116f478d3f2e51203ba603119bbb6d
avitalwerz <avitalw@netapp.com>
2022-09-12 09:21:59 +0300

    change chart version

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "create_namespace" {
 variable "chart_version" {
   type        = string
   description = "Chart version to be deployed."
-  default     = "1.0.1"
+  default     = "1.0.2"
 }
 
 variable "chart_repository" {
