dffe3b3a6492f6c55ec180aae1b92ab5e7020eb0
Baruch Susman <73817458+baruchis123@users.noreply.github.com>
2023-01-31 11:27:07 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "create_namespace" {
 variable "chart_version" {
   type        = string
   description = "Chart version to be deployed."
-  default     = "1.0.8"
+  default     = "1.0.9"
 }
 
 variable "chart_repository" {
