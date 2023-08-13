4b35cd354fab3cf34f6ff5b42ded9d18322bdc2d
Baruch Susman <73817458+baruchis123@users.noreply.github.com>
2023-08-13 15:31:05 +0300

    [src] update helm to version 1.0.4 (#12)

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "create_namespace" {
 variable "chart_version" {
   type        = string
   description = "Chart version to be deployed."
-  default     = "1.0.10"
+  default     = "1.0.12"
 }
 
 variable "chart_repository" {
